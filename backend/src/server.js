78ef21aef16ed0f8528c70aa62524d5aad54e4ed
Omnia Ahmed <omniaahmedhassan48@gmail.com>
2026-06-17 12:36:07 +0300

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./conf/db.js";
 import dns from "dns";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -14,14 +15,24 @@ dns.setServers([
 ])
 
 const app = express();
-const port = process.env.PORT | 5001
+const port = process.env.PORT || 5001
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV !== "production") {
+   app.use(cors({
+      origin: "http://localhost:5173",
+   }));
+}
 app.use(express.json());
 app.use(rateLimiter);
-app.use(cors({
-   origin: "http://localhost:5173",
-}));
+
 app.use("/notes", notesRouter);
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+   app.get("/{*splat}", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+   })
+}
 
 connectDB().then(() => {
    app.listen(port, () => {
